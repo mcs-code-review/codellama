6b8cb61a153918e6c663a13d31923e4171ca7107
Mukhammad Karimov <10654965+heykarimoff@users.noreply.github.com>
2025-03-21 16:38:56 +1100

    reduce time

--- run-34b-instruct-few-shot.sh
+++ run-34b-instruct-few-shot.sh
@@ -35,7 +35,7 @@
 #SBATCH --mail-type=END
 
 # The maximum running time of the job in days-hours:mins:sec
-#SBATCH --time=1-0:0:00
+#SBATCH --time=0-3:0:00
 
 # Standard output and error log
 #SBATCH -o logs/34b-instruct-few-shot-%N.%j.out # STDOUT
